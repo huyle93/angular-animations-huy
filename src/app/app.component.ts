04942064a74bcd42860250532b1ccfc2fc5734fd
huyle93 <huyle.finance@gmail.com>
2018-03-07 21:24:44 -0500

    set up navbar. set up boostrap 4. restructured app components. finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <nav class="navbar navbar-toggleable-sm fixed-top navbar-inverse mb-4" style="background-color:#5c2509;">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#main-nav" aria-controls="main-nav"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">Home Project</a>
+      <div class="collapse navbar-collapse" id="main-nav">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link">Home</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  `,
+  styles: [``]
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
